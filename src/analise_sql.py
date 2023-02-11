800a8e5de5b03549f536cff78f8a8833d223916a
ANJOS, J. S <0rakul0render@gmail.com>
2023-02-11 16:48:43 -0300

    att

---
+++ src/analise_sql.py
@@ -0,0 +1,41 @@
+import psycopg2
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+class sql_analise():
+    def __init__(self):
+        # configuração de credenciais
+        host = 'host.docker.internal'
+        user = 'postgres'
+        password = 'postgrespw'
+        port = '32768'
+
+        self.conn = psycopg2.connect(host=host, user=user, password=password, port=32768, database="postgres")
+    def count_dados(self, dados):
+        cursor = self.conn.cursor()
+        cursor.execute(f"""
+            select count({dados}), {dados} from desenv_processos.processos_movimento pm 
+            group by {dados}
+            """)
+        self.conn.commit()
+        result = cursor.fetchall()
+        df = pd.DataFrame(result, columns=[f'count({dados})', f'{dados}'])
+        cursor.close()
+        print(df)
+        return df
+    def grafico(self, df, dados):
+        df.plot(kind='bar', x=dados, y=f'count({dados})')
+        mean = df[f'count({dados})'].mean()
+        plt.ylim(mean - 1500, mean + 1500)
+        plt.xlabel('staus')
+        plt.ylabel('Contagem')
+        plt.title(f'Contagem de {dados}')
+        plt.axhline(mean, color='red', linestyle='dashed')
+        plt.show()
+
+if __name__ == "__main__":
+    sq = sql_analise()
+    referencia = 'comarca'
+    df = sq.count_dados(referencia)
+    sq.grafico(df, dados=referencia)
