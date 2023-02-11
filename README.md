da4615175b83516ad5d6f8ee9111f858aaf5a325
ANJOS, J. S <0rakul0render@gmail.com>
2023-02-11 17:16:47 -0300

    analise de todas as tabelas

---
+++ README.md
@@ -0,0 +1,1 @@
+# este repositório tem o fundamento de trazer os principais tipos de analises em SQL com Python
--- src/analise_sql.py
+++ src/analise_sql.py
@@ -12,6 +12,17 @@ class sql_analise():
         port = '32768'
 
         self.conn = psycopg2.connect(host=host, user=user, password=password, port=32768, database="postgres")
+    def quais_esquemas_temos(self, schema, tabela):
+        cursor = self.conn.cursor()
+        query = f"SELECT column_name FROM information_schema.columns WHERE table_schema = " \
+                f"'{schema}' AND table_name = '{tabela}'"
+        cursor.execute(query)
+        # Armazena o resultado da consulta em uma lista
+        columns = [row[0] for row in cursor.fetchall()]
+        print(columns)
+        self.conn.commit()
+        cursor.close()
+        return columns
     def count_dados(self, dados):
         cursor = self.conn.cursor()
         cursor.execute(f"""
@@ -24,6 +35,7 @@ class sql_analise():
         cursor.close()
         print(df)
         return df
+
     def grafico(self, df, dados):
         df.plot(kind='bar', x=dados, y=f'count({dados})')
         mean = df[f'count({dados})'].mean()
@@ -36,6 +48,8 @@ class sql_analise():
 
 if __name__ == "__main__":
     sq = sql_analise()
-    referencia = 'comarca'
-    df = sq.count_dados(referencia)
-    sq.grafico(df, dados=referencia)
+    dados_tabela = sq.quais_esquemas_temos('desenv_processos', 'processos_movimento')
+    # para a coluna data, indice 1 cuidado são 10000 itens
+    for colunas in dados_tabela[2:]:
+        df = sq.count_dados(colunas)
+        sq.grafico(df, dados=colunas)
